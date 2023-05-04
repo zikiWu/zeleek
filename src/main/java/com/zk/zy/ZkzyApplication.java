658cfc101bfc878431494493281c34cb5e164509
wuzekun <15521143125@163.com>
2023-05-04 23:45:06 +0800

    init

---
+++ src/main/java/com/zk/zy/ZkzyApplication.java
@@ -0,0 +1,13 @@
+package com.zk.zy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZkzyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZkzyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zk/zy/ZkzyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zk.zy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZkzyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
